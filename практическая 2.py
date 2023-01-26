80fc0d1128127a355f961708c784559dd2a44ae3
MaksTsybizov <123497171+MaksTsybizov@users.noreply.github.com>
2023-01-26 15:18:27 +0200

    Update and rename практическая 2.py to практическая 2.2.py

--- практическая 2.py
+++
@@ -1,9 +0,0 @@
-def reverse(str):  
-    if len(str) == 0: # Checking the lenght of string 
-        return str  
-    else:  
-        return reverse(str[1:]) + str[0]  
-   
-str = "95675"  
-print ("Исходная строка: ", str)    
-print ("Перевернутая строка: ", reverse(str))
