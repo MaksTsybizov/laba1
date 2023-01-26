25b957869fc2e63294950ccf3954a5a755150b89
MaksTsybizov <123497171+MaksTsybizov@users.noreply.github.com>
2023-01-26 15:14:46 +0200

    Update практическая 1.1.py

--- практическая 1.1.py
+++ практическая 1.1.py
@@ -1,3 +1,8 @@
-arr = [1, 2, 3, 10, 20, 23, 43, 5, 6, 7]    # и т.д.
-print(*arr)
-print(*[0 if i < 10 else 1 if i > 20 else i for i in arr])
+from random import randint
+lst = [randint(1,10) for i in range(10)]
+print(lst)
+AAA = [i for i in set(lst) if lst.count(i) > 1]
+if len(AAA) == 0:
+   print("Повторяющихся элементов нет")
+else:
+   print(AAA)
