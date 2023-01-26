516c34a9e0932cf4612ffa026777b893321dda29
MaksTsybizov <123497171+MaksTsybizov@users.noreply.github.com>
2023-01-26 15:16:42 +0200

    Delete практическая 1.py

--- практическая 1.py
+++
@@ -1,8 +0,0 @@
-from random import randint
-lst = [randint(1,10) for i in range(10)]
-print(lst)
-AAA = [i for i in set(lst) if lst.count(i) > 1]
-if len(AAA) == 0:
-   print("Повторяющихся элементов нет")
-else:
-   print(AAA)
